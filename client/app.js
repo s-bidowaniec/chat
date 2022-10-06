44c8204eca24771aa912f3cbfa3e42a011a12b15
Stanislaw Bidowaniec <20791742+s-bidowaniec@users.noreply.github.com>
2022-10-06 17:46:25 +0200

    Add message form

--- client/app.js
+++ client/app.js
@@ -6,14 +6,16 @@ const userNameInput = document.getElementById('username');
 const messageContentInput = document.getElementById('message-content');
 let userName = ''
 
-const validateUserName = (name) => {
+// Validators
+const validate = (name) => {
     const notEmpty = name !== '';
     return notEmpty
 }
 
+// User login form
 const login = (e) => {
     e.preventDefault()
-    if (validateUserName(userNameInput.value)){
+    if (validate(userNameInput.value)){
         userName = userNameInput.value
         loginForm.classList.toggle('show')
         messagesSection.classList.toggle('show')
@@ -21,5 +23,35 @@ const login = (e) => {
         alert("Name is not valid!");
     }
 }
-
 loginForm.addEventListener('submit', login)
+
+// Message form
+const addMessage = (author, message) => {
+    const messageHTML = document.createElement("li");
+    const header = document.createElement("h3");
+    const content = document.createElement("div");
+    messageHTML.classList.add("message", "message--received");
+    if (author === userName) {
+        messageHTML.classList.add("message--self");
+        header.innerText = 'You'
+    } else {
+        header.innerText = author
+    }
+    header.classList.add('message__author')
+    content.classList.add('message__content')
+    content.innerText = message
+    messageHTML.appendChild(header)
+    messageHTML.appendChild(content)
+    messagesList.appendChild(messageHTML)
+}
+const sendMessage = (e) => {
+    e.preventDefault()
+    if (validate(messageContentInput.value)){
+        const message = messageContentInput.value
+        addMessage(userName, message)
+        messageContentInput.value = ''
+    } else {
+        alert("Message is not valid!");
+    }
+}
+addMessageForm.addEventListener('submit', sendMessage)
